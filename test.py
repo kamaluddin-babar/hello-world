3b638c8344925235258873fee13a8104d2f03e91
Mohammad Kamal Uddin <kamaluddin.cse94@gmail.com>
2020-12-31 13:07:21 +0600

    Update test_py
    Just to see how it works.

--- test.py
+++ test.py
@@ -3,4 +3,10 @@ name = input('Enter a your name: ')
 
 x = 10 
 for i in x :
-  print('Congradulations Mr.', name)
+  print('Congratulations Mr.', name)
+  
+while True:
+  xname = input('Enter a valid name: ')
+  if xname == 'done' :
+    break
+  print('Congratulations Mr.', xname)
